e75cbc880b33ee25b446f6d8d08955fe012f63fd
Martin <mr.baratto@unlix.net>
2024-02-06 12:51:39 +0100

    Node.js Backend

---
+++ Backend/index.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs').promises;
+const cors = require('cors');
+
+const app = express();
+const PORT = 3000;
+const ROOFTOP_REVOLUTION_BUILDING_TYPES = ['house'];
+
+app.use(bodyParser.json());
+app.use(cors());
+
+async function getClients(cups) {
+  try {
+    const clients = await fs.readFile('./data/clients.json', 'utf-8');
+    return clients ? JSON.parse(clients).find(client => client.cups === cups) : null;
+  } catch (error) {
+    console.error('Error consulting clients:', error);
+    throw error;
+  }
+}
+
+async function getSupplyPoints(cups) {
+  try {
+    const supplyPoints = await fs.readFile('./data/supply-points.json', 'utf-8');
+    return supplyPoints ? JSON.parse(supplyPoints).find(supplyPoint => supplyPoint.cups === cups) : null;
+  } catch (error) {
+    console.error('Error consulting supply points:', error);
+    throw error;
+  }
+}
+
+app.get('/', (req, res) => {
+  res.send('OK');
+});
+
+const haveDiscount = (supplyPointInfo) => {
+  if (supplyPointInfo.neighbors?.length > 0) {
+    let haveDiscount = true;
+    supplyPointInfo.neighbors.forEach(async (neighbor) => {
+      const neighborSupplyPointInfo = await getSupplyPoints(neighbor);
+      if (neighborSupplyPointInfo.power.p1 >= supplyPointInfo.power.p1 || neighborSupplyPointInfo.power.p2 >= supplyPointInfo.power.p2) {
+        haveDiscount = false;
+      }
+    });
+
+    return haveDiscount;
+  }
+  return false;
+}
+
+app.post('/clients/info', async (req, res) => {
+  try {
+    const { cups } = req.body;
+
+    if (!cups) {
+      return res.status(400).json({ error: 'The "cups" parameter is required' });
+    }
+
+    const clientInfo = await getClients(cups);
+    const supplyPointInfo = await getSupplyPoints(cups);
+
+    const offers = {
+      isRooftopRevolutionAllowed: ROOFTOP_REVOLUTION_BUILDING_TYPES.includes(clientInfo.building_type) && supplyPointInfo.neighbors.length >= 1,
+      discount: haveDiscount(supplyPointInfo) ? 5 : 0,
+    }
+
+    const result = {
+      clientInfo,
+      supplyPointInfo,
+      offers
+    };
+
+    res.json(result);
+  } catch (error) {
+    console.error('Error getting client info:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
